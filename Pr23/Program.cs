456980da3173332284056591118be0823c8751d2
Denis <dnnshlgn@icloud.com>
2022-10-01 13:59:11 +0300

    Pr23

---
+++ Pr23/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("введите N");
+double n = Convert.ToInt32(Console.ReadLine());
+double i = 1;
+while(i <= n)
+{
+    if (i !=n) Console.Write($"{Math.Pow((i),3)},");
+    else
+     Console.Write($"{Math.Pow((i),3)},");
+    i++;
+}
